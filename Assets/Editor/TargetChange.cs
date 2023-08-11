846ace54468db859bf99c847a9e6970c48551784
ddbbm1 <49828646+ddbbm1@users.noreply.github.com>
2023-08-12 04:25:21 +0900

    Add files via upload

---
+++ Assets/Editor/TargetChange.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+        [PostProcessBuild]
+        public static void OnPostprocessBuild(BuildTarget target, string projectPath)
+        {
+            RunPostBuildScript(target: target, projectPath: projectPath);
+        }
+        private static void RunPostBuildScript(BuildTarget target, string projectPath = "")
+        {
+            if (target == BuildTarget.iOS)
+            {
+#if UNITY_IOS
+	            Debug.Log("[Adjust]: Starting to perform post build tasks for iOS platform.");
+	
+	            string xcodeProjectPath = projectPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
+	
+	            PBXProject xcodeProject = new PBXProject();
+	            xcodeProject.ReadFromFile(xcodeProjectPath);
+	
+#if UNITY_2019_3_OR_NEWER
+	            string xcodeTarget = xcodeProject.GetUnityMainTargetGuid();
+#else
+	            string xcodeTarget = xcodeProject.TargetGuidByName("Unity-iPhone");
+#endif
+	            HandlePlistIosChanges(projectPath);
+	
+	xcodeProject.WriteToFile(xcodeProjectPath);
+#endif
+            }
+        }
+#if UNITY_IOS
+   private static void HandlePlistIosChanges(string projectPath)
+    {
+	        var plistPath = Path.Combine(projectPath, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+        var plistRoot = plist.root;
+	plistRoot.SetBoolean("UIFileSharingEnabled",true);
+	plistRoot.SetBoolean("LSSupportsOpeningDocumentsInPlace",true);
+	plistRoot.SetBoolean("UISupportsDocumentBrowser",true);
+
+	
+	Debug.Log("[Adjust]: plist UIFileSharingEnabled,LSSupportsOpeningDocumentsInPlace change.");
+	File.WriteAllText(plistPath, plist.WriteToString());
+	}
+#endif
+}
--- Assets/Editor/ToolKit.cs
+++ Assets/Editor/ToolKit.cs
@@ -3,7 +3,6 @@ using UnityEditor;
 using System.IO;
 using System.Collections;
 using UnityEditor.Callbacks;
-using UnityEditor.iOS.Xcode;
 
 public class ToolKit : EditorWindow 
 {
@@ -112,91 +111,3 @@ public class ToolKit : EditorWindow
     }
     ShowSystemInfo show_system_info_ = new ShowSystemInfo();
 }
-class targetcahge
-{
-    [PostProcessBuild]
-    public static void OnPostprocessBuild(BuildTarget target, string projectPath)
-    {
-        RunPostBuildScript(target: target, projectPath: projectPath);
-    }
-private static void RunPostBuildScript(BuildTarget target, string projectPath = "")
-    {
-        if (target == BuildTarget.iOS)
-        {
-#if UNITY_IOS
-	            Debug.Log("[Adjust]: Starting to perform post build tasks for iOS platform.");
-	
-	            string xcodeProjectPath = projectPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
-	
-	            PBXProject xcodeProject = new PBXProject();
-	            xcodeProject.ReadFromFile(xcodeProjectPath);
-	
-	#if UNITY_2019_3_OR_NEWER
-	            string xcodeTarget = xcodeProject.GetUnityMainTargetGuid();
-	#else
-	            string xcodeTarget = xcodeProject.TargetGuidByName("Unity-iPhone");
-	#endif
-	            HandlePlistIosChanges(projectPath);
-	
-	xcodeProject.WriteToFile(xcodeProjectPath);
-#endif
-	}
-}
-#if UNITY_IOS
-   private static void HandlePlistIosChanges(string projectPath)
-{
-	        var plistPath = Path.Combine(projectPath, "Info.plist");
-        var plist = new PlistDocument();
-        plist.ReadFromFile(plistPath);
-        var plistRoot = plist.root;
-	plistRoot.SetBoolean("UIFileSharingEnabled",true);
-	plistRoot.SetBoolean("LSSupportsOpeningDocumentsInPlace",true);
-	plistRoot.SetBoolean("UISupportsDocumentBrowser",true);
-
-	
-	Debug.Log("[Adjust]: plist UIFileSharingEnabled,LSSupportsOpeningDocumentsInPlace change.");
-	File.WriteAllText(plistPath, plist.WriteToString());
-	}
-#endif
-}
-/*
-public static class XcodeOption
-{
-
-    [PostProcessBuild(999)]
-    public static void OnPostProcessBuild( BuildTarget buildTarget, string path)
-    {
-        if(buildTarget == BuildTarget.iOS)
-        {
-            {
-                string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
-
-                PBXProject pbxProject = new PBXProject();
-                pbxProject.ReadFromFile(projectPath);
-
-                string target = pbxProject.TargetGuidByName("Unity-iPhone");            
-                pbxProject.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
-
-                pbxProject.WriteToFile (projectPath);
-            }
-
-            {
-                string infoPlistPath = path + "/Info.plist";
-
-                PlistDocument plistDoc = new PlistDocument();
-                plistDoc.ReadFromFile(infoPlistPath);
-                if (plistDoc.root != null) {
-                	plistDoc.root.SetBoolean("UIFileSharingEnabled", true);
-			plistDoc.root.SetBoolean("LSSupportsOpeningDocumentsInPlace", true);
-                	plistDoc.WriteToFile(infoPlistPath);
-                }
-                else {
-                    Debug.LogError("ERROR: Can't open " + infoPlistPath);
-                }
-            }
-
-            //ITSAppUsesNonExemptEncryption
-        }
-    }
-	*/
-
